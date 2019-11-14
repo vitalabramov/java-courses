9f1ae7ea49bdc35828d62ec4ffb41bdacafb2f4b
unknown <vitalabramov@gmail.com>
2019-11-14 14:37:09 +0200

    added subtract, multiplication, division, exponent

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,19 @@
 public class Calculate {
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		int subtr = first - second;
+		int mult = first * second;
+		int div = first / second;
+		int exp = 1;
+		for ( int i = 1; i <= second; i++)
+			exp *= first;
+		System.out.println("Sum = " + summ);
+		System.out.println("Subtract = " + subtr);
+		System.out.println("Multiplication = " + mult);
+		System.out.println("Division = " + div);
+		System.out.println("Exponent = " + exp);
 	}
 }
